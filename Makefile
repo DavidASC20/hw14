1e07934fb97ebbf5ddb725526c7305ab33cc7f6d
DavidASC20 <djchong24@gmail.com>
2021-11-09 22:31:50 -0500

    finished

---
+++ Makefile
@@ -0,0 +1,5 @@
+all: hw14.c
+	gcc -o hw14 hw14.c
+
+run:
+	./hw14
---
+++ hw14.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <sys/types.h>
+
+static void batman(int signal){
+    if(signal == SIGINT){
+        int fd = open("info.data", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if(fd == -1){
+            printf("Error, Message is: %s", strerror(errno));
+        }
+
+        char * message = "BAD BAD, PROGRAM EXIT";
+        write(fd, message, strlen(message));
+        exit(0)
+    }else if(signal == SIGUSR1){
+        printf("PPID: %d\n", getppid());
+    }
+}
+
+int main(){
+    signal(SIGINT, batman);
+    signal(SIGUSR1, batman);
+
+    while(1==1){
+        printf("PID: %d\n", getpid());
+        sleep(1);
+    }
+}
